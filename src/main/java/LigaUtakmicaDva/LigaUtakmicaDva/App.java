79ca6bc9af0549eeb073148cdd1e15c35fba17bf
PBojana <bo_sale@hotmail.com>
2017-06-15 17:07:02 +0200

    Commit 1

---
+++ src/main/java/LigaUtakmicaDva/LigaUtakmicaDva/App.java
@@ -0,0 +1,13 @@
+package LigaUtakmicaDva.LigaUtakmicaDva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
